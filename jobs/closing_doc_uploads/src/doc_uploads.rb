335ccfc4a1d9553fdb921ac9553ab913460d558d
kdavis <kdavis@auction.com>
2015-08-04 12:01:08 -0400

    update doc uploads ruby script to follow new directory structure on ftp server

--- jobs/closing_doc_uploads/src/doc_uploads.rb
+++ jobs/closing_doc_uploads/src/doc_uploads.rb
@@ -15,40 +15,39 @@ class DocUploads < Job
 		@@fldMap = {
 			"FHW" => {
 				"title" => "Final HUD Packages",
-				"object" => "Offer__c",
+				"folder" => "Offer",
 				"fields" => ["Final_HUD_Reviewed_Date__c", "Final_HUD_Upload_Date__c"]
 			},
 			"CTU" => {
 				"title" => "Unexecuted Contracts",
-				"object" => "Offer__c",
+				"folder" => "Offer",
 				"fields" => ["Contract_To_Seller__c"]
 			},
 			"CTE" => {
 				"title" => "Executed Contracts",
-				"object" => "Offer__c",
+				"folder" => "Offer",
 				"fields" => ["Date_PSA_Fully_Executed__c"]
 			},
 			"EHUD" => {
-				"title" => "Executed Contracts",
-				"object" => "Offer__c",
+				"title" => "Est. HUD Package",
+				"folder" => "Offer",
 				"fields" => [""]
 			},
 			"PTR" => {
 				"title" => "Property Title Reports",
-				"object" => "Auction__c",
+				"folder" => "Auction",
 				"fields" => ["PTR_Upload_Date__c"]
 			}
 		}
 
 		@@dml_log = {
-	    	"file" => {
-	    		"successes" => [],
-	    		"failures" => []
-	    	},
-	    	"offer" => {
-	    		"successes" => [],
-	    		"failures" => []
-	    	}
+			"auction" => [],
+			"offer" => []
+	    }
+
+	    @@file_log = {
+	    	"processed" => [],
+	    	"failed" => []
 	    }
 		
 		# Restforce.log = true
@@ -59,111 +58,134 @@ class DocUploads < Job
 
 		Net::SFTP.start(@options['sftp']['closing']['host'], @options['sftp']['closing']['username'], :password => @options['sftp']['closing']['password']) do |sftp|
 			files = []
-			offer_nums = []
 
-			# collect offer auto numbers
-			sftp.dir.foreach("/Closing Document Uploads/dropbox") do |file|
+			# iterate through offer dropbox directory
+			sftp.dir.foreach("Offer/dropbox") do |file|
 				if file.name =~ /^*\.pdf$/ then
 
 			    	filename = File.basename(file.name)
 			    	file_key = filename[/-(.*?)\.\w+$/,1]
-			    	
-			    	if @@fldMap.has_key?(file_key) then
-			    		files << filename
-			    		offer_num = filename[/^(.*?)-/,1]
-			    		offer_nums << offer_num if offer_nums.index(offer_num).nil?			    		
+			    	record_id = filename[/^(.*?)-/,1]
+
+					@@file_log["processed"] << filename
+
+			    	if @@fldMap.has_key?(file_key) && !record_id.nil? && (record_id.length == 18 || record_id.length == 15) then
+			    		files << filename			    		
 			    	else
 			    		@logger.info "ERROR: "+filename+" Incorrect naming convention"	
-			    		sftp.rename!("Closing Document Uploads/dropbox/"+filename, "Closing Document Uploads/error/"+filename)
-						@@dml_log["file"]["failures"] << offer_num
+			    		sftp.rename!("Offer/dropbox/"+filename, "Offer/failed/"+filename)
+			    		@@file_log["failed"] << filename
 			    	end
 			    end    	
 		  	end
 
-		  	#map to maintain offer objects to update in case multiple files exist for one offer
+			# iterate through auction dropbox directory
+			sftp.dir.foreach("Auction/dropbox") do |file|
+				if file.name =~ /^*\.pdf$/ then
 
+			    	filename = File.basename(file.name)
+			    	file_key = filename[/-(.*?)\.\w+$/,1]
+			    	record_id = filename[/^(.*?)-/,1]
+
+			    	@@file_log["processed"] << filename
+
+			    	if @@fldMap.has_key?(file_key) && !record_id.nil? && record_id.length > 0 then
+			    		files << filename			    		
+			    	else
+			    		@logger.info "ERROR: "+filename+" Incorrect naming convention"
+			    		sftp.rename!("Auction/dropbox/"+filename, "Auction/failed/"+filename)
+			    		@@file_log["failed"] << filename
+			    	end
+			    end    	
+		  	end
+
+		  	#map to maintain objects for update in case multiple files exist for the same id
 		  	offers_for_update = {}
-
-		  	#collect hash of offer names to ids
-		  	offer_ids = fetch_offer_ids(offer_nums)		  	
+		  	auctions_for_update = {}
 
 		    #post feed items and stamp dates to salesforce
 		    if !files.empty? then
-		    	files.each do |file|		    		
+		    	files.each do |file|
+
+		    		puts file	    		
 
 		    		file_key = file[/-(.*?)\.\w+$/,1]
-		    		offer_num = file[/^(.*?)-/,1]
-		    		offer_id = offer_ids.has_key?(offer_num) ? offer_ids[offer_num] : ''
+		    		record_id = file[/^(.*?)-/,1]
 		    		file_name = @@fldMap[file_key]["title"]
+		    		folder = @@fldMap[file_key]["folder"]
 
-		    		if offer_id.length > 0
-				    	data = sftp.download!("/Closing Document Uploads/dropbox/"+file)
+		    		if record_id.length > 0
+				    	data = sftp.download!(folder+"/dropbox/"+file)
 
 				    	#set the date field on the offer instance
-				    	offer = offers_for_update.has_key?(offer_id) ? offers_for_update[offer_id] : { "Id" => offer_id }
-				    	offers_for_update[offer_id] = offer
+				    	record_for_update = {}
+
+				    	if folder == "Offer" then
+				    		offer = offers_for_update.has_key?(record_id) ? offers_for_update[record_id] : { "Id" => record_id }
+				    		offers_for_update[record_id] = offer
+
+				    		record_for_update = offer
+				    	end
+
+				    	if folder == "Auction" then
+				    		auction = auctions_for_update.has_key?(record_id) ? auctions_for_update[record_id] : { "Id" => record_id }
+				    		auctions_for_update[record_id] = auction
+
+				    		record_for_update = auction
+				    	end
 
 						@@fldMap[file_key]["fields"].each do |fld|
-							offer[fld] = Date.today
+							record_for_update[fld] = Date.today
 						end
 
 				    	#create the feed item
 						begin
-							res = @@client.create!("FeedItem", ParentId: offer_id, ContentData: Base64::encode64(data), ContentFileName: file_name, Body: file_name, Visibility: 'AllUsers')
-							if res.length == 18 then
-								@@dml_log["file"]["successes"] << offer_num
-							end
+							res = @@client.create!("FeedItem", ParentId: record_id, ContentData: Base64::encode64(data), ContentFileName: file_name, Body: file_name, Visibility: 'AllUsers')
 
 						rescue => e
 							puts e.inspect
-							sftp.rename!("Closing Document Uploads/dropbox/"+file, "Closing Document Uploads/error/"+file)
-							@@dml_log["file"]["failures"] << offer_num
+							sftp.rename!(folder+"/dropbox/"+file, folder+"/failed/"+file)
+
+							@@file_log["failed"] << filename
 						end
 					end					
 
 					# move the file to the processed folder
-					sftp.rename!("Closing Document Uploads/dropbox/"+file, "Closing Document Uploads/processed/"+file)
-
-					puts @@dml_log
+					sftp.rename!(folder+"/dropbox/"+file, folder+"/complete/"+file)
 				end
 		    end
 
 		    #update the offer records
-		    offers_for_update.keys.each do |offer_id|
-				offer = offers_for_update[offer_id]
-				puts offer
+		    if !offers_for_update.empty? then
+			    offers_for_update.keys.each do |record_id|
+					offer = offers_for_update[record_id]
 
-				begin
-					res = @@client.update!("Offer__c", offer)
-				rescue => e
-					puts e.inspect
+					begin
+						res = @@client.update!("Offer__c", offer)
+					rescue => e
+						@@dml_log["offer"] << record_id
+						puts e.inspect
+					end
+				end
+			end
+
+			#update the auction records
+		    if !auctions_for_update.empty? then
+			    auctions_for_update.keys.each do |record_id|
+					auction = auctions_for_update[record_id]
+
+					begin
+						res = @@client.update!("Auction__c", auction)
+					rescue => e
+						@@dml_log["auction"] << record_id
+						puts e.inspect
+					end
 				end
 			end
 		end
-	end
 
-	#fetch offer ids filtered by names and return a map of offer name to offer id
-	def fetch_offer_ids(offer_nums)
-
-		offer_hash = {}
-	  	offer_nums_str = ''	  	
-
-	  	begin
-		    offer_nums.each do |num|
-		    	offer_nums_str += (offer_nums_str.length == 0) ? '\''+num+'\'' : ',\''+num+'\''
-		    end
-
-		    if offer_nums_str.length > 0 then
-		    	offers = @@client.query('SELECT Id, Name FROM Offer__c WHERE Name IN ('+offer_nums_str+')')		    	
-
-		    	offers.each do |offer|	    		
-		    		offer_hash[offer.Name] = offer.Id
-		    	end
-		    end
-		rescue => e
-			puts e.inspect
-		end
-
-	    offer_hash
+		# if !@@dml_log.empty? then
+			# @logger.info "ERROR: dml failed for the following record ids| "+@@dml_log
+		# end
 	end
 end
