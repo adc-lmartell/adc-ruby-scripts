f29d11f7a5f1cc6adb0c4b928050fa8cd4456aba
Luke Martell <lmartell@auction.com>
2016-01-15 06:38:31 -0800

    Minor tweaks to regex

--- jobs/seller_zip_exporter/src/seller_zip_exporter.rb
+++ jobs/seller_zip_exporter/src/seller_zip_exporter.rb
@@ -1,4 +1,4 @@
-require "#{ENV['RUNNER_PATH']}/lib/job.rb"
+ require "#{ENV['RUNNER_PATH']}/lib/job.rb"
 require "#{ENV['RUNNER_PATH']}/lib/logger.rb"
 require 'net/sftp'
 require 'date'
@@ -24,26 +24,28 @@ class SellerZipExporter < Job
 		@logger.info "Login successful"	
 
 		zip_path = "#{ENV['ZIP_PATH']}"
-		date_s = Date.today.to_s.gsub("-","")
 
 		appraisal_groups = client.query('SELECT Id, Name FROM CollaborationGroup WHERE Name LIKE \'%Appraisals%\'')
 
 		appraisal_groups.each do |group|
 			name = group.Name
 
-			if !name.match(/^(\D+)\s-\sAppraisals/).nil? then				
-				seller = "#{$1}"
+			unless name.match(/^(\D+)\s-\sAppraisals/).nil? then
+				seller = "#{$1}".gsub(/[^\D]/, "")
 				parentId = group.Id
 
-				zip_folders = client.query("SELECT Id, Title, ContentDescription, ContentFileName, ContentType, ContentData, Type from FeedItem WHERE ParentID = '#{parentId}'")
+				zip_folders = client.query("SELECT Id, Title, ContentType, ContentData, CreatedDate from FeedItem WHERE ParentID = '#{parentId}'")
 				zip_folders.each do |zip|
 					if zip.ContentType == "application/zip" then
-						zip_filename = zip_path+"/#{seller}_#{date_s}_#{zip.Title}.zip"
-						puts zip_filename
+						created_date = zip.CreatedDate.split(".")[0].gsub(/[\-\:]/, "")
+
+						zip_filename = "#{zip_path}/#{seller}_#{created_date}_#{zip.Title}.zip"
 						
 						content = client.get zip.ContentData
 						f = File.new(zip_filename, "wb")
 						f.write(content.body)
+
+						@logger.info "FeedItem ID: #{zip.Id}"
 					end
 				end				
 			end
