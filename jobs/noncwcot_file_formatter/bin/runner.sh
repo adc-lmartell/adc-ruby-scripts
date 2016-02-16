5366e83352011c03c6c58ed593cd77374eb7aedb
Luke Martell <lmartell@auction.com>
2016-02-15 19:53:47 -0800

    Removed credentials

--- jobs/noncwcot_file_formatter/bin/runner.sh
+++ jobs/noncwcot_file_formatter/bin/runner.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export RUNNER_PATH=/Users/Kelsen23/Documents/Ruby/Scripts/adc-ruby-scripts
+export RUNNER_PATH=/path/to/adc-ruby-scripts
 export SELLER_MATRIX=SellerCodeMatrix.csv
 export SELLER_DATATAPE=SellerDataTape.csv
 export ASSET_LOAD=NonCWCOT-AssetLoad.csv
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-cd ~/Documents/Ruby/Scripts/adc-ruby-scripts/jobs/noncwcot_file_formatter/bin/
+cd ~/path/to/adc-ruby-scripts/jobs/noncwcot_file_formatter/bin/
 ruby noncwcot_file_formatter-runner.rb
