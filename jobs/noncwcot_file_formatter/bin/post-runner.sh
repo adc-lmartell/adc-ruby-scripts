943236a03659ad0c319c517ad444997a71b0238b
Luke Martell <lmartell@auction.com>
2016-02-16 07:02:20 -0800

    Added pre-post runners

--- jobs/noncwcot_file_formatter/bin/post-runner.sh
+++ jobs/noncwcot_file_formatter/bin/post-runner.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-export RUNNER_PATH=
+export RUNNER_PATH=/path/to/adc-ruby-scripts
 export SF_PROD_URL=
 export SF_PROD_USER=
 export SF_PROD_PWD=
 export SF_PROD_TOKEN=
 export SF_PROD_CID=
 export SF_PROD_CLS=
-export SELLER_MATRIX=SellerCodeMatrix.csv
-export SELLER_DATATAPE=SellerDataTape.csv
 export ASSET_LOAD=NonCWCOT-AssetLoad.csv
+export REO_AUC_LOAD=NonCWCOT-ReoAucLoad.csv
+export JOB_ORDER=Post
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
