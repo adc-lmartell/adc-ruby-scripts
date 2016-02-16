53b986e0db24af723ed92aec6253ff00f3647e62
Luke Martell <lmartell@auction.com>
2016-02-16 07:05:04 -0800

    Resolve conflict in runner

--- jobs/noncwcot_file_formatter/bin/post-runner.sh
+++ jobs/noncwcot_file_formatter/bin/post-runner.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 export RUNNER_PATH=/path/to/adc-ruby-scripts
-<<<<<<< HEAD:jobs/noncwcot_file_formatter/bin/post-runner.sh
 export SF_PROD_URL=
 export SF_PROD_USER=
 export SF_PROD_PWD=
 export SF_PROD_TOKEN=
 export SF_PROD_CID=
 export SF_PROD_CLS=
-=======
 export SELLER_MATRIX=SellerCodeMatrix.csv
 export SELLER_DATATAPE=SellerDataTape.csv
->>>>>>> origin/master:jobs/noncwcot_file_formatter/bin/runner.sh
 export ASSET_LOAD=NonCWCOT-AssetLoad.csv
 export REO_AUC_LOAD=NonCWCOT-ReoAucLoad.csv
 export JOB_ORDER=Post
