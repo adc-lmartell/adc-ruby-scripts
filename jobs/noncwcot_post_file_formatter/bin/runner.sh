ac00b12d92abf9d06684b36130001ccf7ae554b0
Luke Martell <lmartell@auction.com>
2016-02-15 19:53:01 -0800

    Removed credentials

--- jobs/noncwcot_post_file_formatter/bin/runner.sh
+++ jobs/noncwcot_post_file_formatter/bin/runner.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-export RUNNER_PATH=/Users/Kelsen23/Documents/Ruby/Scripts/adc-ruby-scripts
+export RUNNER_PATH=/path/to/adc-ruby-scripts
 export SF_PROD_URL=
-export SF_PROD_USER=lmartell@auction.com
-export SF_PROD_PWD=L!11kM234
-export SF_PROD_TOKEN=5o1MxJrZZQ0KupiMMthv2xzG
-export SF_PROD_CID=3MVG9iTxZANhwHQt0j0oOGc9BodRJK74qbBIve_2lqR_vPBrpJMmz_scGKKTjqztvfmtPtBSLTfH5.tW3YVg3
-export SF_PROD_CLS=2798385460059144056
+export SF_PROD_USER=
+export SF_PROD_PWD=
+export SF_PROD_TOKEN=
+export SF_PROD_CID=
+export SF_PROD_CLS=
 export ASSET_LOAD=NonCWCOT-AssetLoad.csv
 export REO_AUC_LOAD=NonCWCOT-ReoAucLoad.csv
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-cd ~/Documents/Ruby/Scripts/adc-ruby-scripts/jobs/noncwcot_post_file_formatter/bin/
+cd ~/path/to/adc-ruby-scripts/jobs/noncwcot_post_file_formatter/bin/
 ruby noncwcot_post_file_formatter-runner.rb
