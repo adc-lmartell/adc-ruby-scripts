3089416354975b03e083a32557eee48e46e83aa4
Luke Martell <lmartell@auction.com>
2016-02-15 19:51:59 -0800

    Removed SFDC System Variables

--- jobs/noncwcot_file_formatter/bin/runner.sh
+++ jobs/noncwcot_file_formatter/bin/runner.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-export RUNNER_PATH=
-export SF_PROD_URL=
-export SF_PROD_USER=
-export SF_PROD_PWD=
-export SF_PROD_TOKEN=
-export SF_PROD_CID=
-export SF_PROD_CLS=
+export RUNNER_PATH=/Users/Kelsen23/Documents/Ruby/Scripts/adc-ruby-scripts
 export SELLER_MATRIX=SellerCodeMatrix.csv
 export SELLER_DATATAPE=SellerDataTape.csv
 export ASSET_LOAD=NonCWCOT-AssetLoad.csv
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-cd ~/path/to/adc-ruby-scripts/jobs/noncwcot_file_formatter/bin/
+cd ~/Documents/Ruby/Scripts/adc-ruby-scripts/jobs/noncwcot_file_formatter/bin/
 ruby noncwcot_file_formatter-runner.rb
