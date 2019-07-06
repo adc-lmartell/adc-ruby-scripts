955912a9a748ee68ee05a627ab91bfe00d90a352
Luke Martell <lmartell@auction.com>
2019-07-06 08:03:34 -0700

    HTTP callouts for UAA and Offers

--- jobs/offer_mcds_true_up/src/offer_mcds_true_up.rb
+++ jobs/offer_mcds_true_up/src/offer_mcds_true_up.rb
@@ -1,5 +1,7 @@
 require "#{ENV['RUNNER_PATH']}/lib/job.rb"
-
+require 'net/https'
+require 'json'
+require 'uri'
 
 class OfferMcdsTrueUp < Job
 
@@ -18,11 +20,13 @@ class OfferMcdsTrueUp < Job
 			client = restforce_client[:client]
 
 			# Pull the offers that need updated
-			offers = client.query("SELECT Id, Property_ID__c, Transaction_ID__c, Online_Contract_Form_Complete__c, MCDS_Sent_to_Buyer_Date__c, MLH_DocuSign_Status__c, MLH_DocuSign_Envelope_ID__c, MLH_DocuSign_Status_Date__c, Unsigned_Docusign_Contract_URL__c, Signed_Docusign_Contract_URL__c, Original_MCDS_to_Buyer_Date__c FROM Offer__c WHERE Property_ID__c != null AND Transaction_ID__c = null AND Is_Winning_Bid__c = 'Yes' AND Bid_Date_Time__c >= LAST_N_DAYS:120 AND Auction_Event__r.Program_Enrollment__r.MCDS_Enabled__c = true LIMIT 1")
+			offers = client.query("SELECT Id, Auction_Event__r.MLH_Property_ID__c, Transaction_ID__c, Online_Contract_Form_Complete__c, MCDS_Sent_to_Buyer_Date__c, MLH_DocuSign_Status__c, MLH_DocuSign_Envelope_ID__c, MLH_DocuSign_Status_Date__c, Unsigned_Docusign_Contract_URL__c, Signed_Docusign_Contract_URL__c, Original_MCDS_to_Buyer_Date__c FROM Offer__c WHERE Property_ID__c != null AND Transaction_ID__c = null AND Is_Winning_Bid__c = 'Yes' AND Bid_Date_Time__c >= LAST_N_DAYS:120 AND Auction_Event__r.Program_Enrollment__r.MCDS_Enabled__c = true LIMIT 1")
 
 			unless offers.size == 0
 				offers.each do |offer|
-					contract_info = get_contract_status(offer.Property_ID__c)
+					contract_info = get_contract_status({
+						'listing_id' => offer.Auction_Event__r.MLH_Property_ID__c
+					})
 					unless contract_info.nil?
 						save_sf_record(offer, contract_info)
 					end
@@ -38,10 +42,59 @@ class OfferMcdsTrueUp < Job
 
 	private
 
-	def get_contract_status(listing_id)
-		unless @auth_token.nil?
+	def format_url_params(params)
+		params.map { |k,v| "#{k}=#{v}" }.join('&')
+	end
 
+	def get_auth_token(params)
+		auth_token = nil
+
+		headers = {
+			'Content-Type' => 'application/json',
+			'Accept' => 'application/json'
+		}
+		uri = URI.parse("#{@options['uaa']['endpoint']}?#{format_url_params(params)}")
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		request = Net::HTTP::Post.new(uri.request_uri, headers)
+		request.body = @options['uaa']['grant_type']
+		response = http.request(request)
+		auth_response = JSON.parse(response.body)
+
+		if auth_response.has_key?('access_token')
+			auth_token = auth_response['access_token']
 		end
+
+		return auth_token
+	end
+
+	def get_contract_status(params)
+		contract_info = nil
+
+		if @auth_token.nil?
+			@auth_token = get_auth_token({
+				'grant_type' => @options['uaa']['grant_type'],
+				'client_id' => @options['uaa']['client_id'],
+				'client_secret' => @options['uaa']['client_secret']
+			})
+		end
+
+		unless @auth_token.nil?
+			headers = {
+				'Authorization' => "Bearer #{@auth_token}",
+				'Content-Type' => 'application/json',
+				'Accept' => 'application/json'
+			}
+			uri = URI.parse("#{@options['contract']['offers_endpoint']}?#{format_url_params(params)}")
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Get.new(uri.request_uri, headers)
+			response = http.request(request)
+			offer_response = JSON.parse(response.body)
+			puts offer_response
+		end
+
+		return contract_info
 	end
 
 	def save_sf_record(offer, contract_status)
